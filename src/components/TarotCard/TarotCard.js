9786ab062ba0ee6e1ce54f22c7f22a588931d8d0
Liam Carr <liam@liamcarr.com>
2019-02-07 21:43:54 -0500

    add React hook to tarotcard

--- src/components/TarotCard/TarotCard.js
+++ src/components/TarotCard/TarotCard.js
@@ -1,52 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./TarotCard.module.css";
 
 const TarotCard = props => {
-  function importAll(r) {
+  const importAll = r => {
     return r.keys().map(r);
-  }
+  };
 
-  const cups = importAll(
-    require.context("../../images/RWS/cups", false, /\.(png|jpe?g|svg)$/)
-  );
-  const pentacles = importAll(
-    require.context("../../images/RWS/pentacles", false, /\.(png|jpe?g|svg)$/)
-  );
-  const swords = importAll(
-    require.context("../../images/RWS/swords", false, /\.(png|jpe?g|svg)$/)
-  );
-  const wands = importAll(
-    require.context("../../images/RWS/wands", false, /\.(png|jpe?g|svg)$/)
-  );
-  const majors = importAll(
-    require.context("../../images/RWS/majors", false, /\.(png|jpe?g|svg)$/)
-  );
+  const [images, setImages] = useState({
+    cups: importAll(
+      require.context("../../images/RWS/cups", false, /\.(png|jpe?g|svg)$/)
+    ),
+    wands: importAll(
+      require.context("../../images/RWS/wands", false, /\.(png|jpe?g|svg)$/)
+    ),
+    swords: importAll(
+      require.context("../../images/RWS/swords", false, /\.(png|jpe?g|svg)$/)
+    ),
+    pentacles: importAll(
+      require.context("../../images/RWS/pentacles", false, /\.(png|jpe?g|svg)$/)
+    ),
+    majors: importAll(
+      require.context("../../images/RWS/majors", false, /\.(png|jpe?g|svg)$/)
+    )
+  });
 
   let card = null;
-  console.log(props.card);
-  if (props.card.major) card = majors[props.card.number];
+  if (props.card.major) card = images.majors[props.card.number];
   else {
     switch (props.card.value) {
       case "Cups":
-        card = cups[props.card.number - 1];
+        card = images.cups[props.card.number - 1];
         break;
       case "Pentacles":
-        card = pentacles[props.card.number - 1];
+        card = images.pentacles[props.card.number - 1];
         break;
       case "Swords":
-        card = swords[props.card.number - 1];
+        card = images.swords[props.card.number - 1];
         break;
       case "Wands":
-        card = wands[props.card.number - 1];
+        card = images.wands[props.card.number - 1];
         break;
+      default:
+        console.log("error - card suite does not exist");
     }
   }
 
-  console.log(card);
-
   return (
     <div style={{ display: props.card.value ? "block" : "none" }}>
-      <img src={card} className={styles.TarotCard} />
+      <img src={card} alt={props.card.value} className={styles.TarotCard} />
     </div>
   );
 };
