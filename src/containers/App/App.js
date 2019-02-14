4cd3636a86299496bc8836420ec0f16b2f427ad7
Liam Carr <liam@liamcarr.com>
2019-02-13 20:22:36 -0500

    Fix the toolbar to top of browser

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -11,7 +11,6 @@ class App extends Component {
       <BrowserRouter>
         <div className={styles.App}>
           <Layout />
-          {/* <DailyDrawJournal /> */}
           <Dashboard />
         </div>
       </BrowserRouter>
